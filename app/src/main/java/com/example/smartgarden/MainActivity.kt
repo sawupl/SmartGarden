83a96826b610e0ed49596172061c9cae7da85ed6
sawupl <suraroslav390@gmail.com>
2023-12-16 18:53:01 +0400

    Notification Update

--- app/src/main/java/com/example/smartgarden/MainActivity.kt
+++ app/src/main/java/com/example/smartgarden/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.smartgarden
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.smartgarden.notification.NotificationReceiver
+import com.example.smartgarden.notification.Notification
+import com.example.smartgarden.notification.channelID
+import com.example.smartgarden.notification.messageExtra
+import com.example.smartgarden.notification.notificationID
+import com.example.smartgarden.notification.titleExtra
 import java.util.Calendar
 
 
@@ -14,33 +22,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        createNotificationChannel()
         scheduleNotification()
     }
 
+    private fun createNotificationChannel() {
+        val name = "Notif Channel"
+        val desc = "A Description of the Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(channelID, name, importance)
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        channel.description = desc
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    @SuppressLint("ScheduleExactAlarm")
     private fun scheduleNotification() {
-        // Получение AlarmManager
+        val intent = Intent(applicationContext, Notification::class.java)
+        val title = "TITLE"
+        val message = "Message"
+        intent.putExtra(titleExtra, title)
+        intent.putExtra(messageExtra, message)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        // Создание Intent для BroadcastReceiver
-        val intent = Intent(this, NotificationReceiver::class.java)
-        val pendingIntent =
-            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        // Установка повторяющегося будильника на каждый день в 8:00
         val calendar = Calendar.getInstance()
         calendar.set(Calendar.HOUR_OF_DAY, 8)
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
-
-        val alarmStartTime = calendar.timeInMillis
-
-        val interval = AlarmManager.INTERVAL_DAY // повтор каждый день
-
-        alarmManager.setRepeating(
+        val time = calendar.timeInMillis
+        alarmManager.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
-            alarmStartTime,
-            interval,
+            time,
             pendingIntent
         )
     }
