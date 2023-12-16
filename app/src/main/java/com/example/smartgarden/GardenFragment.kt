355c5e86bc0560afa48a9bd65a330e19c492bb3b
vbifarik <99973240+vbifarik@users.noreply.github.com>
2023-12-16 12:43:44 +0400

    Датчик света

--- app/src/main/java/com/example/smartgarden/GardenFragment.kt
+++ app/src/main/java/com/example/smartgarden/GardenFragment.kt
@@ -1,5 +1,10 @@
 package com.example.smartgarden
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,16 +14,23 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartgarden.databinding.FragmentGardenBinding
 
-class GardenFragment : Fragment() {
+class GardenFragment : Fragment(),SensorEventListener{
     private lateinit var binding: FragmentGardenBinding
     private lateinit var viewModel: GardenViewModel
     private var gardenId: String? = null
+
+    private lateinit var sensorManager: SensorManager
+    private var sensor:Sensor?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this, ViewModelFactory())[GardenViewModel::class.java]
         gardenId = arguments?.getString("gardenId")
         viewModel.getGarden(gardenId.toString())
+
+
+        sensorManager= (requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
     }
 
 
@@ -35,5 +47,22 @@ class GardenFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        binding.currentLight.text = event!!.values[0].toString()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }
